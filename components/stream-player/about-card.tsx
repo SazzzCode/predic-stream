a61ab8c94dcc2f7e23a2da4c2503c35b1be0700c
SazzzCode <miltonenrique7777@gmail.com>
2025-03-19 08:01:12 -0400

    easily

--- components/stream-player/about-card.tsx
+++ components/stream-player/about-card.tsx
@@ -26,7 +26,7 @@ const AboutCard = ({
 			<div className="group rounded-xl bg-background p-6 lg:p-10 flex flex-col gap-y-3">
 				<div className="flex items-center justify-between">
 					<div className="flex items-center gap-x-2 font-semibold text-lg lg:text-2xl">
-						About {hostName}
+						Sobre {hostName}
 						<VerifiedMark />
 					</div>
 					{isHost && (
